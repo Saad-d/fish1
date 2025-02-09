4c08ad2800b11be9f80eb91d4698a1c5464ab870
Saad-d <saadaltafkhan456@gmail.com>
2025-02-09 19:30:27 +0530

    changes in fish_purchase.js

--- fish1/fish/doctype/fish_purchase_invoice/fish_purchase_invoice.js
+++ fish1/fish/doctype/fish_purchase_invoice/fish_purchase_invoice.js
@@ -1,8 +1,49 @@
 // Copyright (c) 2025, Saad and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Fish Purchase Invoice", {
-// 	refresh(frm) {
+frappe.ui.form.on("Fish Purchase Invoice", {
 
-// 	},
-// });
+    onload(frm){
+        if (frm.is_new()) {
+            let now = frappe.datetime.now_datetime();
+            let [date, time] = now.split(' ');
+            frm.set_value('date', date);          
+            frm.set_value('posting_time', time);  
+        }
+    },
+});
+
+frappe.ui.form.on("Expenses", {  // 
+    rupee: function(frm, cdt, cdn) {
+        update_total_expenses(frm);
+    }
+});
+
+frappe.ui.form.on("Payments", {  // 
+    rupes: function(frm, cdt, cdn) {
+        update_total_payment(frm);
+    }
+});
+
+function update_total_expenses(frm) {
+    let dca = 0;
+    $.each(frm.doc.table_sovl || [], function(i, d) {
+        dca += flt(d.rupee);
+    });
+    frm.set_value("total_expenses", dca).then(() => {
+        frm.refresh_field("total_expenses");
+    });
+}
+
+function update_total_payment(frm) {
+    let dca = 0;
+    $.each(frm.doc.payments || [], function(i, d) {
+        dca += flt(d.rupes);
+    });
+    frm.set_value("total_payments", dca).then(() => {
+        frm.refresh_field("total_payments")
+    });
+}
+
+
+
--- fish1/fish/doctype/fish_purchase_invoice/fish_purchase_invoice.py
+++ fish1/fish/doctype/fish_purchase_invoice/fish_purchase_invoice.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2025, Saad and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class FishPurchaseInvoice(Document):
-	pass
+	def validate(self):
+		total_expenses = sum(frappe.utils.flt(row.rupee) for row in self.table_sovl)
+		self.total_expenses = total_expenses
+
+		total_payment = sum(frappe.utils.flt(row.rupes) for row in self.payments)
+		self.total_payments = total_payment
